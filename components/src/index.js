997bbde1b01278d20df2791bc6ef66b4877bf604
Ebenex2020 <ebenex20202@gmail.com>
2023-05-02 12:58:58 +0200

    Update

--- components/src/index.js
+++ components/src/index.js
@@ -2,28 +2,35 @@ import React from "react";
 import ReactDOM from "react-dom/client";
 import CommentDetails from "./Components/CommentDetails/CommentDetails";
 import { faker } from "@faker-js/faker";
+import Card from "./Components/CommentDetails/Card";
 
 const App = () => {
   return (
     <div className="ui container comments ">
-      <CommentDetails
-        author="Samuel"
-        time="3:00pm"
-        myPic={faker.image.avatar()}
-        text="Nice blog Post!"
-      />
-      <CommentDetails
-        author="Seun"
-        time="12:00pm"
-        myPic={faker.image.avatar()}
-        text="Better blog Post!"
-      />
-      <CommentDetails
-        author="Tobi"
-        time="9:00pm"
-        myPic={faker.image.avatar()}
-        text="Best blog Post!"
-      />
+      <Card>
+        <CommentDetails
+          author="Samuel"
+          time="3:00pm"
+          myPic={faker.image.avatar()}
+          text="Nice blog Post!"
+        />
+      </Card>
+      <Card>
+        <CommentDetails
+          author="Seun"
+          time="12:00pm"
+          myPic={faker.image.avatar()}
+          text="Better blog Post!"
+        />
+      </Card>
+      <Card>
+        <CommentDetails
+          author="Tobi"
+          time="9:00pm"
+          myPic={faker.image.avatar()}
+          text="Best blog Post!"
+        />
+      </Card>
     </div>
   );
 };
